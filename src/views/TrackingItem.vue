8592dfd25179cf95e400750b378ca3968a7705f0
dannyindratjuatja <dindratjuatja@gmail.com>
2020-09-20 21:40:07 +0700

    bug fixed scroll and dynamic route tracking id

---
+++ src/views/TrackingItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tracking">
+        <label class="tracking__label">tracking id: {{$route.params.id}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.tracking{
+    padding: 10rem;
+
+    &__label{
+        font-size: 48px;
+        color: black;
+    }
+}
+</style>
