c2637cd5f9edf5ee449be2902840afb260eeb995
Jacob Andrean <andreanjacob99@gmail.com>
2020-09-28 03:15:29 +0700

    update about page

--- src/views/users/About.vue
+++ src/views/users/About.vue
@@ -1,88 +1,95 @@
 <template>
   <div class="about">
     <div>
-      <img class ="about__imageabout" src="@/assets/about/aboutimage.jpg"/>
+      <img class ="about__banner" src="@/assets/about/aboutimage.jpg"/>
     </div>
     
     <div>
       <img class="about__logo" src="@/assets/about/logo2rev.png" />
     </div>
 
-    <div>
-      <label class="about__desc">Jakarta’s premier wine bar and shop,</label>
-      <label class="about__desc">boasting an inimitably cozy dining and drinking experience. </label>
+    <div class="about__title">
+      <label>Supreme's premier restaurant</label>
+      <label>Boasting an inimitably cozy dining experience. </label>
     </div>
 
     <div class="about__column">
         <div class="about__left">
-            <label class="about__desc"> text1</label>
-            <label class="about__desc"> text2 blabla</label>
-            <label class="about__desc"> text3blablabla</label>
+            <label> text1</label>
+            <label> text2 blabla</label>
+            <label> text3blablabla</label>
         </div>
 
         <div class="about__right">
-            <label class="about__desc"> text1</label>
-            <label class="about__desc"> text2blabla</label>
-            <label class="about__desc"> text3blablabla</label>
+            <label> text1</label>
+            <label> text2blabla</label>
+            <label> text3blablabla</label>
         </div>
+    </div>
+
+    <div>
+      <img class ="about__banner" src="@/assets/about/aboutimage.jpg"/>
+    </div>
+
+    <div class="about__desc">
+      <label>A restaurant and bar inside a wine shop, Supreme's Kitchen is a sophisticated dining destination conveniently located in Jakarta's hip Thamrin and SCBD area. Our cellar is home to the largest wine selection in the country while the kitchen focuses on an extensive menu comprising of European and Asian favorites.</label>
+      <span>View Menu</span>
     </div>
 
   </div>
 </template> 
 <style lang="scss" scoped>
-.about{
+
+.about {
   padding:0rem 0em ;
 
-    &__title{
-        font-size: 50px;
-        font-weight: 550;
-        margin: 35px 0px 50px 7vw;
-    }
-  
-    &__logo{
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 5em;
-        padding-bottom:1em ;
+  &__logo{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5em;
+    padding-bottom: 1em ;
+  }
 
-    }
+  &__banner{
+    width: 100vw;
+    margin: -5rem 0em;
+  }
 
-    &__imageabout{
-      margin: -5rem 0em;
-    }
-
-    &__desc{
-      font-size:20px;
-      display:flex;
-      justify-content: center;
-      //margin-right:auto;
-   }
+  &__title{
+    font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
   &__column{
-    display:grid;
-    margin-top:4em;
-    padding-left:5em;
-    padding-right:5em;
+    display: grid;
+    margin: 10em 0 10em 0;
+    padding: 5em 5em;
     grid-template-columns: repeat(2,1fr);
   }
 
-   &__left{
-        display:flex;
-        flex-direction: column;
-        justify-content: center;
-        //border-left: 1px solid $light-grey;
-       // padding: 10px 150px 30px 2.5rem;
-    }
+  &__left{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-    &__right{
-        display:flex;
-        flex-direction: column;
-        justify-content: center;
-       border-left: 1px solid $light-grey;
-       // padding: 10px 150px 30px 2.5rem;
-    }
+  &__right{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-left: 1px solid $light-grey;
+    //height: 50em;
+  }
 
+  &__desc{
+    font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
 }
 </style>
