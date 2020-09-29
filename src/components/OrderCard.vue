9592fe39367653fc4478815d206489eabb6df65f
unknown <harryronaldo17@gmail.com>
2020-09-29 11:56:59 +0700

    Ordercard and Order

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="order">
+        <div class="order__image"><!--  Gambar-->
+            Gambar Jacob
+        </div>
+        <div>
+            <div class="order__details">
+                <p>Harga Diri Jacob Andrean</p>
+                <p>ZWE 1.000.000</p>
+            </div>
+            <div class="order__iconbar">
+                <div class="order__note">
+                    <font-awesome-icon icon="edit" class="order__note__icon" />
+                </div>
+                <div class="order__quantity">
+                    <font-awesome-icon icon="minus" class="order__quantitiy__icon minus" v-on:click="quantity=quantity-1"/>
+                    <label class="menu__quantitiy__number">{{quantity}}</label>
+                    <font-awesome-icon icon="plus" class="order__quantitiy__icon plus" v-on:click="quantity=quantity+1"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            quantity: 0,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.order{
+    display: flex;
+
+    &__details{
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+    &__iconbar{
+        display: flex;
+    }
+} 
+</style>
--- src/views/users/Order.vue
+++ src/views/users/Order.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="order">
-        this is order 
+        <div class="menu__category">
+            <div class="menu__title-container">
+               <span class="menu__title">Jacob</span>
+            </div>
+        </div>
+        <div class="order">
+            <OrderCard/>
+        </div>
     </div>
 </template>
 
 <script>
+import OrderCard from '@/components/OrderCard.vue';
 export default {
-    
+    components:{
+        OrderCard
+    },
+
 }
 </script>
 
 <style lang="scss" scoped>
+@import "@/styles/menu.scss";
+
 .order{
-    padding: 5rem 1.2rem;
+    padding: 5em 10vw;
+
 }
 </style>
