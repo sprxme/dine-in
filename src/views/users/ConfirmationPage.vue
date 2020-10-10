c7375c12213a5a10ebc39221e5b2b7b026588214
vctxr <victorsamuelcuaca131@gmail.com>
2020-10-10 23:36:26 +0700

    Added automatic copy order token to clipboard

--- src/views/users/ConfirmationPage.vue
+++ src/views/users/ConfirmationPage.vue
@@ -3,11 +3,19 @@
         <h1 class="confirm__title">Thank you for your order!</h1>
 
         <h1 class="confirm__title2">Here is your token.</h1>
-        <h1 class="confirm__gentoken">{{token}}</h1>
+        <h1 class="confirm__gentoken" ref="token">{{token}}</h1> 
+
         <router-link class = "confirm__link" :to="'/track/' +token">
             <span class="confirm__btn">Track Order</span>
             <font-awesome-icon icon="chevron-right" class="confirm__icon" />  
         </router-link>
+
+        <b-modal class="confirm__alert" id="alert" hide-footer title="Token copied to clipboard!">
+            <p class="confirm__alert__label">You can use this token to track your order's progress.</p>
+            <div class="confirm__alert__buttons">
+                <span class="confirm__alert__button primary-button" @click="$bvModal.hide('alert')">Ok</span>
+            </div>
+        </b-modal>
     </div>
 
 
@@ -17,7 +25,18 @@
 export default {
    props: {
        token: String
-   }
+   },
+   methods: {
+       doCopy() {
+            navigator.clipboard.writeText(this.$refs.token.innerHTML)
+                .then(() => this.$bvModal.show('alert'))
+        }
+    },
+    created: function () {
+        this.$nextTick(function () {
+            this.doCopy()
+        })
+    }
 }
 </script>
 
@@ -31,10 +50,10 @@ export default {
     
     &__title{
         text-align: center;
-        width: 502px;
+        // width: 502px;
         font-size: 50px;
-        margin-bottom: 70px;
-        margin-top:40px;
+        margin-bottom: 80px;
+        margin-top:0px;
     }
 
     &__title2{
@@ -44,12 +63,17 @@ export default {
         margin-bottom: 10px;
     }
 
+    &__token {
+        position: relative;
+        display: flex;
+        align-items: center;
+    }
+
     &__gentoken{
         text-align: center;
-        font-size: 20px;
-        width: 220px;
+        font-size: 50px;
+        font-weight: 700;
         margin-bottom: 60px;
-        color: $light-grey;
     }
 
     &__label{
@@ -71,19 +95,42 @@ export default {
     }
 
     &__icon{
-        margin-top: 4px;
+        margin-top: 1px;
     }
 
     &__btn {
         text-align: center;
-        padding: 0em 0.5em;
+        padding: 0em 0.3em;
         //font-weight: 500;
         font-size:20px;
+    }
+
+    &__alert{
+        &__label{
+            font-size: .95rem;
+            color: $subtitle-text;
+            margin-bottom: 1.5rem;
         }
 
-    &__icon{
-        margin-top: 4px;
-    }    
+        &__buttons{
+            display: flex;
+            justify-content: flex-end;
+
+            .cancel{
+                color: $text;
+                font-weight: 500;
+
+                &:hover{
+                cursor: pointer;
+                }
+            }
+        }
+
+        &__button{
+            padding: .3em 1.5em;
+            border-radius: 3px;
+        }
+    }
 }
 </style>
 
