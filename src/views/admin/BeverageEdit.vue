c944635d907fb1c3c7be43967ba02b4e5498d26e
dannyindratjuatja <dindratjuatja@gmail.com>
2020-10-10 18:10:08 +0700

    Add beverage and food add (admin)

--- src/views/admin/BeverageEdit.vue
+++ src/views/admin/BeverageEdit.vue
@@ -4,7 +4,7 @@
         <div class="menu__category" v-for="category in allDrinkCategories" :key="category.id">
             <div class="menu__title-container">
                 <span class="menu__title">{{category.name}}</span>
-                <span class="menu__add primary-button">
+                <span class="menu__add primary-button" v-b-modal.modal-beverage>
                     <label class="menu__add__label">Add</label>  
                     <font-awesome-icon icon="plus" class="menu__tocart__icon"/> 
                 </span>
@@ -12,6 +12,40 @@
             <div class="menu__cards">
                 <MenuCardEdit v-for="drink in sortCategory(category.name)" :key="drink.id" :menu="drink" :type="'beverage'"/>
             </div>
+            <b-modal id="modal-beverage" centered hide-footer title="Add Beverage">
+                <div class="menu__modal">
+                    <div class="custom__input">
+                        <span class = "custom__input-row">
+                            <input class="field menu__modal__input" type="text" required>
+                            <span class="placeholder menu__modal__placeholder">Name</span>
+                        </span>
+                    </div>
+                    <div class="custom__input">
+                        <span class = "custom__input-row">
+                            <input class="field menu__modal__input" type="text" required>
+                            <span class="placeholder menu__modal__placeholder">Price</span>
+                        </span>
+                    </div>
+                </div>
+                <div class="menu__catselect">
+                    <b-form-select v-model="selected" class="menu__catselect__select">
+                        <b-form-select-option :value="null" disabled class="menu__catselect__select__title">Category</b-form-select-option>
+                        <b-form-select-option v-for="option in allDrinkCategories" :key="option.id" :value="option.name">
+                            {{option.name}}
+                        </b-form-select-option>
+                    </b-form-select>
+                </div>
+                <div class="custom__input input__textarea">
+                    <span class = "custom__input-row">
+                        <textarea class="menu__modal__input custom__textarea" type="text" required />
+                        <span class="placeholder__textarea menu__modal__placeholder">Description</span>
+                    </span>
+                </div>
+                <div class="menu__modal__buttongroup">
+                    <span class="menu__modal__buttongroup__button destructive-secondary">Cancel</span>
+                    <span class="menu__modal__buttongroup__button primary-button">Save</span>
+                </div>
+            </b-modal>
         </div>
     </div>
 </template>
@@ -21,6 +55,11 @@ import MenuCardEdit from '@/components/admin/AdminMenuCard';
 import { mapGetters } from 'vuex';
 
 export default {
+    data: function(){
+        return{
+            selected: null,
+        }
+    },
     methods:{
         sortCategory: function(category){
             return this.allDrinks.filter(function(drink){
--- src/views/admin/FoodEdit.vue
+++ src/views/admin/FoodEdit.vue
@@ -4,7 +4,7 @@
         <div class="menu__category" v-for="category in allFoodCategories" :key="category.id">
             <div class="menu__title-container">
                 <span class="menu__title">{{category.name}}</span>
-                <span class="menu__add primary-button" v-b-modal.modal-add>
+                <span class="menu__add primary-button" v-b-modal.modal-add-food>
                     <label class="menu__add__label">Add</label>  
                     <font-awesome-icon icon="plus" class="menu__tocart__icon"/> 
                 </span>
@@ -13,15 +13,38 @@
                 <MenuCardEdit v-for="food in sortCategory(category.name)" :key="food.id" :menu="food" :type="'food'"/>
             </div>
         </div>
-        <b-modal id="modal-add" centered hide-footer title="Add Food">
-            <div>
-                <label>Name</label>
+        <b-modal id="modal-add-food" centered hide-footer title="Add Food">
+            <div class="menu__modal">
+                <div class="custom__input">
+                    <span class = "custom__input-row">
+                        <input class="field menu__modal__input" type="text" required>
+                        <span class="placeholder menu__modal__placeholder">Name</span>
+                    </span>
+                </div>
+                <div class="custom__input">
+                    <span class = "custom__input-row">
+                        <input class="field menu__modal__input" type="text" required>
+                        <span class="placeholder menu__modal__placeholder">Price</span>
+                    </span>
+                </div>
             </div>
-            <div>
-                <label>Price</label>
+            <div class="menu__catselect">
+                <b-form-select v-model="selected" class="menu__catselect__select">
+                    <b-form-select-option :value="null" disabled class="menu__catselect__select__title">Category</b-form-select-option>
+                    <b-form-select-option v-for="option in allFoodCategories" :key="option.id" :value="option.name">
+                        {{option.name}}
+                    </b-form-select-option>
+                </b-form-select>
             </div>
-            <div>
-                <label>Description</label>
+            <div class="custom__input input__textarea">
+                <span class = "custom__input-row">
+                    <textarea class="menu__modal__input custom__textarea" type="text" required />
+                    <span class="placeholder__textarea menu__modal__placeholder">Description</span>
+                </span>
+            </div>
+            <div class="menu__modal__buttongroup">
+                <span class="menu__modal__buttongroup__button destructive-secondary">Cancel</span>
+                <span class="menu__modal__buttongroup__button primary-button">Save</span>
             </div>
         </b-modal>
     </div>
@@ -32,6 +55,11 @@ import MenuCardEdit from '@/components/admin/AdminMenuCard';
 import { mapGetters } from 'vuex';
 
 export default {
+    data: function(){
+        return{
+            selected: null,
+        }
+    },
     methods:{
         sortCategory: function(category){
             return this.allFoods.filter(function(food){
@@ -42,7 +70,7 @@ export default {
     components:{
         MenuCardEdit
     },
-    computed: mapGetters(['allFoods', 'allFoodCategories'])
+    computed: mapGetters(['allFoods', 'allFoodCategories']),
 }
 </script>
 
