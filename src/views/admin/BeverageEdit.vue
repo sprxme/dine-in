4316039c049091233059df50b3d4f260fd9d795a
dannyindratjuatja <dindratjuatja@gmail.com>
2020-10-10 22:53:25 +0700

    Remove add float button (admin page)

--- src/views/admin/BeverageEdit.vue
+++ src/views/admin/BeverageEdit.vue
@@ -18,42 +18,39 @@
             </div>
         </div>
         <b-modal id="modal-beverage" centered hide-footer title="Add Beverage">
-                <div class="menu__modal">
-                    <div class="custom__input">
-                        <span class = "custom__input-row">
-                            <input class="field menu__modal__input" type="text" required>
-                            <span class="placeholder menu__modal__placeholder">Name</span>
-                        </span>
-                    </div>
-                    <div class="custom__input">
-                        <span class = "custom__input-row">
-                            <input class="field menu__modal__input" type="text" required>
-                            <span class="placeholder menu__modal__placeholder">Price</span>
-                        </span>
-                    </div>
-                </div>
-                <div class="menu__catselect">
-                    <b-form-select v-model="selected" class="menu__catselect__select">
-                        <b-form-select-option :value="null" disabled class="menu__catselect__select__title">Category</b-form-select-option>
-                        <b-form-select-option v-for="option in allDrinkCategories" :key="option.id" :value="option.name">
-                            {{option.name}}
-                        </b-form-select-option>
-                    </b-form-select>
-                </div>
-                <div class="custom__input input__textarea">
+            <div class="menu__modal">
+                <div class="custom__input">
                     <span class = "custom__input-row">
-                        <textarea class="menu__modal__input custom__textarea" type="text" required />
-                        <span class="placeholder__textarea menu__modal__placeholder">Description</span>
+                        <input class="field menu__modal__input" type="text" required>
+                        <span class="placeholder menu__modal__placeholder">Name</span>
                     </span>
                 </div>
-                <div class="menu__modal__buttongroup">
-                    <span class="menu__modal__buttongroup__button destructive-secondary">Cancel</span>
-                    <span class="menu__modal__buttongroup__button primary-button">Save</span>
+                <div class="custom__input">
+                    <span class = "custom__input-row">
+                        <input class="field menu__modal__input" type="text" required>
+                        <span class="placeholder menu__modal__placeholder">Price</span>
+                    </span>
                 </div>
-            </b-modal>
-         <span class="menu__add__float primary-button" v-b-modal.modal-beverage> 
-            <font-awesome-icon icon="plus" class="menu__tocart__icon__float" style="margin-right:0px"/> 
-        </span>
+            </div>
+            <div class="menu__catselect">
+                <b-form-select v-model="selected" class="menu__catselect__select">
+                    <b-form-select-option :value="null" disabled class="menu__catselect__select__title">Category</b-form-select-option>
+                    <b-form-select-option v-for="option in allDrinkCategories" :key="option.id" :value="option.name">
+                        {{option.name}}
+                    </b-form-select-option>
+                </b-form-select>
+            </div>
+            <div class="custom__input input__textarea">
+                <span class = "custom__input-row">
+                    <textarea class="menu__modal__input custom__textarea" type="text" required />
+                    <span class="placeholder__textarea menu__modal__placeholder">Description</span>
+                </span>
+            </div>
+            <div class="menu__modal__buttongroup">
+                <span class="menu__modal__buttongroup__button destructive-secondary">Cancel</span>
+                <span class="menu__modal__buttongroup__button primary-button">Save</span>
+            </div>
+        </b-modal>
     </div>
 </template>
 
