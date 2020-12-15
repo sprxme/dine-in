44a6734660348b9f5dd604ed3c8663ec905bda17
vctxr <victorsamuelcuaca131@gmail.com>
2020-12-15 09:01:03 +0700

    built for prod

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const history = require('connect-history-api-fallback')
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 const cors = require('cors')
-dotenv.config()
+// dotenv.config()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 const staticFileMiddleware = express.static(__dirname + '/../dist')
 const staticImageMiddleware = express.static(__dirname + '/../server/uploads')
 
